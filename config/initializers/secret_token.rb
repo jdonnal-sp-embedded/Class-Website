faa0a10c119852ba49dbbf2e8eb15f45da12d9d8
jdonnal <jdonnal@jdonnal-VirtualBox.(none)>
2012-01-11 22:53:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '8d31687756aac62234051bee48a67a75768215f17945d64324d2d1038440bfbf0b96201fff2eb08fb267e28d9d4eafb1cd62b874fbd5f7b1e52ff62aaeb4f0a4'
