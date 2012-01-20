b5cb4eda4ea8ebf9ec99e90f78a85a13182de7c3
John <jdonnal@gmail.com>
2012-01-19 19:40:07 -0500

    changed readme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,12 @@ Website::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+config.action_mailer.default_url_options={:host=>'localhost:3000'}
+config.action_mailer.delivery_method=:smtp
+config.action_mailer.perform_deliveries=false
+config.action_mailer.raise_delivery_errors=true
+config.action_mailer.default :charset => "utf-8"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
